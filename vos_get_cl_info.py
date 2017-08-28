4eae6ef812c341908ea197306bac11f385b6018b
John Vasconcelos <john.vasconcelos@harmonicinc.com>
2017-08-27 21:10:20 -0700

    vos_get_cl_info.py - Corrected list parameters

--- vos_get_cl_info.py
+++ vos_get_cl_info.py
@@ -17,10 +17,10 @@ vos_session = vos.vos_get_session("dfw.ote@gmail.com","dfwote*1")
 
 vosrt = "vosdashboard.dfwcpcgreen.ds.dtvops.net"
 
-cl_list = vos.os_get_cl_list(vosrt,vos_session)
+cl_list = vos.vos_get_cl_list(vosrt,vos_session)
 
 
 #cl_list.append({'clid':clid , 'clname':clname , 'clip':clip , 'clstate':clstate , 'clrate':clrate})
 for cl in cl_list:
 
-    print "%s,%s,%s,%s" %(cl['name'] , cl['clip'] , cl['clstate'] , cl['clrate'])
+    print "%s,%s,%s,%s" %(cl['clname'] , cl['clip'] , cl['clstate'] , cl['clrate'])
